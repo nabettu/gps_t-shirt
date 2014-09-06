9a3547ebdf3be0960cb82c6ae90045fb10aad8f4
tatsuaki_w <tatsuaki.w@gmail.com>
2014-09-07 02:23:14 +0900

    上の文字表示

--- gps.js
+++ gps.js
@@ -4,12 +4,19 @@
 //
 //////////////////////////////////////////////////
 
-$(window).load(function() {
- 		 update_img();
-});
-
 var img = new Image();
 img.src = "t-shirt.jpg";
+
+$(window).load(function() {
+	var canvas = document.getElementById('a_canvas');
+	if (canvas.getContext) {
+		var context = canvas.getContext('2d');
+
+	   	context.drawImage(img, 0, 0);
+	   }
+	}
+);
+
 
 function update_img(){
 	//描画コンテキストの取得
@@ -19,60 +26,37 @@ function update_img(){
 
 	   	context.drawImage(img, 0, 0);
 		context.textAlign = "center";
-		context.font = "40pt Arial";
-		context.fillText("loading...", 340, 340);
-/*
-	   	var inputname = document.listname_form.listname_text.value;
+		var fontsize =10;
+		context.font = fontsize + "pt Arial";
 
-	   	if (inputname == "") {
-	   		inputname = document.listname_form.listname_text.placeholder;
-	   	};
+		context.fillStyle = 'rgba(0, 0, 0, 1)';
+		context.fillRect(340-(fontsize*15), 155, fontsize*30, fontsize+10)
 
-		context.font = "20pt Arial";
-		context.fillStyle = 'rgba(255, 255, 255, 1)';
-		context.strokeStyle = 'rgba(255, 255, 255, 1)';
-		context.shadowColor = 'black';
-		context.shadowOffsetX = 2;
-	  	context.shadowOffsetY = 2;
-  		context.shadowBlur = 4;
-		context.textAlign = "center";
-		context.fillText(inputname, 400, 380); */
 
-	   	context.drawImage(img, 0, 0);
-	};
-};
-
-$(function(){
-	//GPS情報取得を開始
-	$('#start_gps').click(function(){
+//位置情報取得
 		navigator.geolocation.watchPosition(
 			function(position){
-				/*
-				$('#latitude').html(position.coords.latitude); //緯度
-				$('#longitude').html(position.coords.longitude); //経度
-				*/
 
+//forsquareAPI
 				var url="https://api.foursquare.com/v2/venues/search";
 				var id="ZFOPLOQQS0IU10UA322AWCDEU5NL5VGDXSR4QIFYFLNZAYXR"
 				var secret="1PZPDQPDILWYPSHMZ0QOR0QP5AJLBSGNRD24AHAYXARB1UEG"
 
 				$.get(
 				    url,
-				    {ll : position.coords.latitude+","+position.coords.longitude, client_id : id, client_secret:secret,v:"20140905"},
+				    {ll : position.coords.latitude+","+position.coords.longitude, client_id : id, client_secret:secret,v:"20140905", limit:1},
 				    function(data) {
 				    	var	result =data.response.venues; 
-//						console.log(data.response.venues);
-						var list ="";  
-						for(var i=0;i<30;i++){
-							console.log(result[i]);
-							list += "<td>"+result[i].name+"</td>";
-						}
-						//結果を出力
-//						$(".results").innerHTML = list;
+						console.log(result[0].name);
+
+						context.fillStyle = 'rgba(255, 255, 255, 1)';
+						context.fillText(result[0].name, 340, 170);
 				    }
 				);
 
 			}
 		);
-	});
-});
+
+	   	//context.drawImage(img, 0, 0);
+	};
+};
